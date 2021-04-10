722c1323b4f82cf04692b7fa6ba5f6be4a13a98b
Lloyd <dcostalloyd90@gmail.com>
2021-04-10 09:35:06 +0530

    Test commit

---
+++ app/src/main/java/com/xyz/tic_tac_toe_masai/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xyz.tic_tac_toe_masai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
